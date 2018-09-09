edbfee000781ec1b81ef6660b9731b4b31c20395
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-09 17:45:12 -0400

    connected CheeseList to the store and mapped cheeses part of the state to cheeses prop, dispatched fetchCheeses method in componentDidMouint method of CheeseList component

--- src/components/cheese-list.js
+++ src/components/cheese-list.js
@@ -1,14 +1,39 @@
 import React from 'react';
+import {fetchCheeses} from '../actions.cheese';
+import {connect} from 'react-redux';
 
-
-export default function CheeseList (props) {
-    const listedCheeses = props.cheeses.map((cheese, i) => {
-        return (<li key={i}>{cheese}</li>)
-    });
-    return (
-        <ul>
-            {listedCheeses}
-        </ul>
-    );
+export class CheeseList extends React.Component {
+    renderResults() {
+        if(this.props.loading) {
+            return <li>Loading</li>;
+        }
+        if(this.props.error) {
+            return <li>Error</li>;
+        }
+        const listedCheeses = props.cheeses.map((cheese, i) => {
+            return (<li key={i}>{cheese}</li>)
+        });
+        return (listedCheeses);
+    }
+    componentDidMount() {
+        console.log(fetchCheeses());
+        this.props.dispatch(fetchCheeses());
+    }
+    render() {
+        console.log(this.renderResults());
+        return (
+            <ul>
+                {this.renderResults()}
+            </ul>
+        );
+    }
 }
+
+const mapStateToProps = (state) => ({
+    cheeses: state.cheeses,
+    loading: state.loading,
+    error: state.error
+})
+
+export default connect(mapStateToProps)(CheeseList);
 
