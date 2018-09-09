eceb9dc85c5b31f5b333b113cc756a90a7728c0f
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-09 17:42:23 -0400

    wrapped component in React Redux Provider component so it has access to the store

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import reducer from './reducers/cheese';
+import {Provider} from 'react-redux';
 import CheeseList from './components/cheese-list.js';
 import './index.css';
 
+const store = createStore(reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk));
+
+
 ReactDOM.render(
-    <CheeseList cheeses={[
-        "Bath Blue",
-        "Barkham Blue",
-        "Buxton Blue"
-    ]} />,
+    <Provider store={store}>
+        <CheeseList />
+    </Provider>,
     document.getElementById('root')
 );
 
