65790d3fe9623935ffd60b48e0a255d9441b4983
Jeff Maxwell <maxjeffwell@gmail.com>
2018-09-09 16:50:16 -0400

    added React CheeseList component

---
+++ src/components/cheese-list.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default function CheeseList (props) {
+    const listedCheeses = props.cheeses.map((cheese, i) => {
+        return (<li key={i}>{cheese}</li>)
+    });
+    return (
+        <ul>
+            {listedCheeses}
+        </ul>
+    );
+}
+
